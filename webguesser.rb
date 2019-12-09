7719edfe2163ad1af9d15731b3b0d53076575eee
EC2 Default User <ec2-user@ip-172-31-46-173.us-east-2.compute.internal>
2019-12-09 21:08:26 +0000

    Finish basic app

--- webguesser.rb
+++ webguesser.rb
@@ -4,6 +4,23 @@ require 'sinatra/reloader'
 number = rand(100)
 
 get '/' do
-    erb :index, :locals => {:number => number}
+    
+    guess = params["guess"].to_i
+    msg = check_guess(guess,number)
+    erb :index, :locals => {:number => number, :msg => msg}
+end
 
+
+def check_guess(guess, number)
+    if guess > number + 5
+        "Way too high"
+    elsif guess < (number -5)
+        "Way too low"
+    elsif guess > number
+       "Too high"
+    elsif guess < number
+      "Too low"
+    else
+         "You are correct, the number is #{number}"
+    end
 end
